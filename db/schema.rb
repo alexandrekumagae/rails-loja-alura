eb591ad7c0a86851a1206d38fd9661f767fcd85a
Alexandre Kumagae <alexandrekumagae.dev@gmail.com>
2024-05-08 12:22:47 -0300

    chore: finish module 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_195155) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_08_142844) do
+  create_table "departamentos", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
     t.text "descricao"
@@ -18,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_195155) do
     t.integer "quantidade"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "departamento_id"
   end
 
 end
